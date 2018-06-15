7eb598fca5fd90e3c60dfbf3c1c01da6fcdceb81
Adult <Adult@DESKTOP-3ULAN2D>
2018-06-15 10:34:44 +0900

    로그인 기능만 완성

--- 99_OfficeKnower/src/main/java/com/kh/ok/member/controller/MemberController.java
+++ 99_OfficeKnower/src/main/java/com/kh/ok/member/controller/MemberController.java
@@ -1,11 +1,14 @@
 package com.kh.ok.member.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.ok.member.model.service.MemberService;
@@ -14,11 +17,13 @@ import com.kh.ok.member.model.vo.Member;
 @SessionAttributes({"memberLoggedIn"})
 @Controller
 public class MemberController {
-	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
 	@Autowired
 	private MemberService memberService;
+	
 	@Autowired
-	BCryptPasswordEncoder bcryptPasswordEncoder;
+	private BCryptPasswordEncoder bcryptPasswordEncoder;
 	
 	
 	@RequestMapping("/member/memberEnroll.do")
@@ -30,7 +35,7 @@ public class MemberController {
 	@RequestMapping("/member/memberLogin.do")
 	public ModelAndView memberLogin(@RequestParam String userId, @RequestParam String password) {
 		ModelAndView mav = new ModelAndView();
-		Member m = memberService.selectOne(userId);
+		Member m = memberService.selectUserId(userId);
 		
 		String msg = "";
 		String loc = "/";
@@ -51,5 +56,14 @@ public class MemberController {
 		//view단지정
 		mav.setViewName("common/msg");
 		return mav;
+	}
+	@RequestMapping("/member/memberLogout.do")
+	public String memerLogout(SessionStatus sessionStatus) {
+		if(logger.isDebugEnabled())
+			logger.debug("로그아웃 요청");
+		//현재 session상태를 끝났다고 마킹함.
+		if(!sessionStatus.isComplete())
+			sessionStatus.setComplete();
+		return "redirect:/";
 	}
 }
