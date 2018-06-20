331dc585624509c95fad0c106fdc7d876f182fb4
heewoon1204 <heewoon1204@naver.com>
2018-06-20 13:21:28 +0900

    일정추가 버튼 완성

--- 99_OfficeKnower/src/main/java/com/kh/ok/calendar/controller/CalendarContoroller.java
+++ 99_OfficeKnower/src/main/java/com/kh/ok/calendar/controller/CalendarContoroller.java
@@ -28,7 +28,7 @@ public class CalendarContoroller {
 	@RequestMapping("/cal/cal.do")
 	public String demo() {
 		
-		return"calendar/cal-mini";
+		return"calendar/test";
 	}
 	
 	@RequestMapping("/cal/calTest.do")
