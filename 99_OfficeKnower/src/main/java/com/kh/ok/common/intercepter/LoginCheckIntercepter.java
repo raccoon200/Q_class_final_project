8e33f7a8b56248fa7f520501f59d016320417f34
DESKTOP-43K9OEC\USER <raccoon200@naver.com>
2018-06-21 10:53:50 +0900

    loginCheckIntercepter 추가

---
+++ 99_OfficeKnower/src/main/java/com/kh/ok/common/intercepter/LoginCheckIntercepter.java
@@ -0,0 +1,26 @@
+package com.kh.ok.common.intercepter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginCheckIntercepter extends HandlerInterceptorAdapter{
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		
+		if(session.getAttribute("memberLoggedIn") == null /*|| !((Member)session.getAttribute("memberLoggedIn")).getUserId().equals(request.getParameter("userId"))*/) {
+			
+			request.setAttribute("msg", "잘못된 경로입니다.");
+			request.setAttribute("loc", "/");
+			request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp").forward(request, response);
+			return false;
+		}
+		
+		return super.preHandle(request, response, handler);
+		
+	}
+}
